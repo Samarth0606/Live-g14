9640dc1d10f6e5096ccb1d20e65e9ce0e98f9cfe
Samarth0606 <officialdevelopersamarth@gmail.com>
2022-12-20 11:19:16 +0530

    restful crud done

--- RESTful/index.js
+++ RESTful/index.js
@@ -1,20 +1,26 @@
 let express = require('express');
 let app = express();
 let path = require('path');
+const { v4: uuid } = require('uuid');
+let methodOverride = require('method-override');
+
+// middleware for using methodOverride
+app.use(methodOverride('_method'));
+
 
 let comments = [
     {
-        id:0,
+        id:uuid(),
         username:"Samarth",
         comment:"chitkara students are nice X 1"
     },
     {
-        id:1,
+        id:uuid(),
         username:"Akhilesh",
         comment:"chitkara students are nice X 2"
     },
     {
-        id:2,
+        id:uuid(),
         username:"Rohan Malik",
         comment:"chitkara students are nice X 3"
     }
@@ -44,7 +50,8 @@ app.get('/comment/new' , (req,res)=>{
 app.post('/comments' , (req,res)=>{
     // console.log(req.body);
     let {username , comment } = req.body;
-    comments.push({username , comment , id:comments.length})
+    // comments.push({username , comment , id:comments.length})
+    comments.push({username , comment , id:uuid()})
     // res.send("data sent succesfully");
     res.redirect('/comments');
 })
@@ -53,11 +60,41 @@ app.post('/comments' , (req,res)=>{
 app.get('/comments/:commentId' , (req,res)=>{
     // console.log(req.params);
     let { commentId } = req.params;
-    let foundComment = comments.find((comment)=>{return comment.id===parseInt(commentId)})
+    // let foundComment = comments.find((comment)=>{return comment.id===parseInt(commentId)})
+    let foundComment = comments.find((comment)=>{return comment.id===(commentId)})
     // console.log(foundComment);
     // res.send("params found succesfully");
     // res.render('show' ,{foundComment} );
     res.render('show' ,{comment : foundComment} );
+})
+
+
+// task 4: to create a form for editing/updating an existing comment
+app.get('/comments/:commentId/edit' , (req,res)=>{
+    let { commentId } = req.params;
+    let foundComment = comments.find((comment)=>{return comment.id===(commentId)})
+    res.render('edit' , {foundComment});
+})
+
+// task 4 cont. : to show the updated version of the comment
+app.patch('/comments/:commentId' , (req,res)=>{
+    let { commentId } = req.params;
+    let foundComment = comments.find((comment)=>{return comment.id===(commentId)})
+    // console.log(foundComment);
+    console.log(req.body);
+    let {comment} = req.body;
+    foundComment.comment = comment;
+    // res.send("patch req sent successfully");
+    res.redirect('/comments');
+})
+
+// task 5: deleting  a particular comment
+app.delete('/comments/:commentId' , (req,res)=>{
+    let { commentId } = req.params;
+    let newCommentArray = comments.filter((comment)=>{return comment.id !== commentId})
+    comments = newCommentArray;
+    res.redirect('/comments');
+
 })
 
 
